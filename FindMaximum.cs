bdaa17d9fef1d5f02c113842ec430e3e0ded42a4
pj2802 <42446681+pj2802@users.noreply.github.com>
2018-08-16 19:14:17 +0200

    Initial commit

---
+++ FindMaximum.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 3, 9, 5, 2, 10, 9, 18 };
+        int maxNumber = numbers.Max();
+        Console.WriteLine(maxNumber);
+        Console.ReadLine();
+    }
+}
